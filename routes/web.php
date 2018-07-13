a5df1ceeec2579f6b5f528069b7455e708890b29
thaoratngau <nmwthao96@gmail.com>
2018-07-13 17:19:50 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/home', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
